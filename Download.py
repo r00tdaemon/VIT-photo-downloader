cfd9aff2c2a7bb3eb6da41e32dc2b08b83665e87
ujjwal96 <jwel96@outlook.com>
2016-10-28 23:53:55 +0530

    Coded to python3

--- Download.py
+++ Download.py
@@ -1,12 +1,12 @@
-import urllib
+import urllib.request
 
-cookie=raw_input("Enter session cookie: ")
-regno=raw_input("Enter registration number: ")
+cookie=input("Enter session cookie: ")
+regno=input("Enter registration number: ")
 url='https://vtop.vit.ac.in/student/view_photo_2.asp?rgno='+regno   
 name=regno+'.jpg'
-img = urllib.URLopener()
+img = urllib.request.URLopener()
 img.addheader('Cookie',cookie)
 if ('image/jpg' in str(img.open(url).info())):
     img.retrieve(url,name)
 else:
-    print "Invalid number"
+    print ("Invalid number")
